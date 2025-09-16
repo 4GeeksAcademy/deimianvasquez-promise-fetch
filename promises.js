2a1c833e635dec3f763131acc1c3980cfc833b42
Deimian Vásquez <deimianvasquez@gmail.com>
2025-09-16 00:32:57 +0000

    fetching

---
+++ promises.js
@@ -0,0 +1,3 @@
+console.log("Hola 1")
+alert("Hola desde el alert")
+console.log("Hola 2")
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,66 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const [characters, setCharacters] = useState([])
+	const [loading, setLoading] = useState(false)
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+	// .then
+	// const getCharacter = () => {
+	// 	fetch("https://rickandmortyapi.com/api/character")
+	// 		.then((response) => response.json())
+	// 		.then((deimian) => setCharacters(deimian.results))
+	// 		.catch((err) => console.log(err))
+	// }
+
+
+	const getCharacter = async () => {
+		try {
+			setLoading(true)
+			const response = await fetch("https://rickandmortyapi.com/api/character")
+			const data = await response.json()
+
+			if (response.ok) {
+				setCharacters(data.results)
+			}
+
+		} catch (error) {
+			console.log(error)
+		} finally {
+			setLoading(false)
+		}
+	}
+
+
+	useEffect(() => {
+		getCharacter()
+	}, [])
+
+	return (
+		<>
+			<div className="container">
+				<div className="row">
+					<div className="col-12">
+						<ul>
+							{
+								loading ?
+									<h1>Cargandooooooo....</h1> :
+									characters.map((item) => {
+										const { id, name, species } = item
+										return (
+											<li key={id}>
+												{`Hola ¿qué tal ${name}? Mi especie es ${species}`}
+											</li>
+										)
+									})
+							}
+
+						</ul>
+					</div>
+				</div>
+			</div>
+		</>
 	);
 };
 
